618d0eae4ce7e1e25813d295a788fa6a7ceaa24f
Mike Rapoport <rppt@kernel.org>
2025-10-31 13:20:52 +0200

    Update text to 2025
    Signed-off-by: Mike Rapoport <rppt@kernel.org>

--- bbb.py
+++ bbb.py
@@ -17,7 +17,7 @@ import config
 #
 # We may eventually want some more flexibility here.
 #
-Welcome = '''Welcome to the Linux Plumbers Conference 2024 %s!
+Welcome = '''Welcome to the Linux Plumbers Conference 2025 %s!
 <br><br>
 Please remember that the <a
 href="https://lpc.events/event/17/page/214-anti-harassment-policy"
